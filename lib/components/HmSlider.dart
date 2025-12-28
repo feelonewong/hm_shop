08528eb08ade88d6e7a0fee21f440aba5aa743a3
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2025-12-28 19:26:16 +0800

    实现轮播图效果

--- lib/components/HmSlider.dart
+++ lib/components/HmSlider.dart
@@ -1,7 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HmSlider extends StatefulWidget {
-  const HmSlider({super.key});
+  final bannerList;
+  const HmSlider({super.key, required this.bannerList});
 
   @override
   State<HmSlider> createState() => _HmSliderState();
@@ -9,13 +11,27 @@ class HmSlider extends StatefulWidget {
 
 // 首页轮播图组件
 class _HmSliderState extends State<HmSlider> {
+  Widget _getSlider() {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return CarouselSlider(
+      items: List.generate(widget.bannerList.length, (int index) {
+        return Image.network(
+          widget.bannerList[index].imgUrl,
+          fit: BoxFit.cover,
+          width: screenWidth,
+        );
+      }),
+      options: CarouselOptions(
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 2),
+        height: 240,
+        viewportFraction: 1,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(color: Colors.blue),
-      child: Text("轮播图组件", style: TextStyle(color: Colors.white, fontSize: 18)),
-    );
+    return Stack(children: [_getSlider()]);
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -4,6 +4,7 @@ import 'package:hm_shop/components/HmHot.dart';
 import 'package:hm_shop/components/HmMoreList.dart';
 import 'package:hm_shop/components/HmSlider.dart';
 import 'package:hm_shop/components/HmSuggestion.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,9 +14,26 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id: "1",
+      imgUrl:
+          'https://images.unsplash.com/photo-1761839262867-af53d08b0eb5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8',
+    ),
+    BannerItem(
+      id: "2",
+      imgUrl:
+          'https://images.unsplash.com/photo-1761839258513-099c3121d72d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ),
+    BannerItem(
+      id: "3",
+      imgUrl:
+          'https://images.unsplash.com/photo-1761839262867-af53d08b0eb5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8',
+    ),
+  ];
   List<Widget> _getScrollViewChildren() {
     return [
-      SliverToBoxAdapter(child: HmSlider()),
+      SliverToBoxAdapter(child: HmSlider(bannerList: _bannerList)),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
       SliverToBoxAdapter(child: HmCategory()),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
